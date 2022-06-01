573f613791136896276b6df48518f09bf63d57c5
luisgrange <luisfernando237grange@outlook.com>
2022-06-01 09:15:22 -0300

    Primeira rota

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3333, ()=> {console.log('HTTP server running!')})
+app.get('/', (req, res) => {
+    return res.send('Hello world');
+});
+
+app.listen(3333, ()=> 
+{console.log('HTTP server running!\nhttp://localhost:3333')})
