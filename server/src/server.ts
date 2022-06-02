2fa75eaa3b76caca49dd0f58efdc32a028b9a71d
luisgrange <luisfernando237grange@outlook.com>
2022-06-02 09:25:33 -0300

    Retornando conteudo criado

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,24 @@
 import express from 'express';
+import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/feedbacks', (req, res) => {
-    console.log(req.body);
+app.post('/feedbacks', async (req, res) => {
+
+    //desestruturando 
+    const {type, comment, screenshot} = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    return res.status(201).json({data: feedback});
 });
 
 app.listen(3333, ()=> 
