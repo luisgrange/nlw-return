17d224a01c98809522d4574f4559810ece569379
luisgrange <luisfernando237grange@outlook.com>
2022-06-07 09:24:14 -0300

    Adicionando classes

--- server/src/repositories/prisma/prisma-feedbacks-repository.ts
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -2,7 +2,7 @@ import { prisma } from '../../prisma';
 import { FeedbackCreateData, FeedbacksRepository } from '../feedbacks-repository';
 
 export class PrismaFeedbackRepository  implements FeedbacksRepository{
-    async create(data: FeedbackCreateData){
+    async create({type, comment, screenshot}: FeedbackCreateData){
         await prisma.feedback.create({
             data: {
                 type,
